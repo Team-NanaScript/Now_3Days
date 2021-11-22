6afe11f9b1bce7f51ea099f7c4123346eafc90e4
Eungyeol41 <iniziato@naver.com>
2021-11-22 14:55:35 +0900

    Project Start

---
+++ app/src/main/java/com/now/three_days/CalenderActivity.kt
@@ -0,0 +1,11 @@
+package com.now.three_days
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calender)
+    }
+}
