0c1d7d1bd95b0c0b3fd447eade8c0f66f3b8a076
Eungyeol41 <iniziato@naver.com>
2021-12-01 14:45:40 +0900

    fragment로 변경 완료!

---
+++ app/src/main/java/com/now/three_days/MainListFragment.kt
@@ -0,0 +1,31 @@
+package com.now.three_days
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainListFragment()
+    }
+
+    private lateinit var viewModel: MainListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
