ad3d34adf1c73e14db3760821149b61e556188d2
Nanask <ybhag1005@daum.net>
2021-12-06 02:44:50 +0900

    cat

---
+++ app/src/main/java/com/now/three_days/ui/main/ListAdapter.kt
@@ -0,0 +1,38 @@
+package com.now.three_days.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.now.three_days.databinding.ItemMainListBinding
+
+
+class ListAdapter(mainFragment: MainFragment) : RecyclerView.Adapter<ListAdapter.ListViewHolder>(){
+    // private val 를 사용해야 가져와서 사용할 수 있음
+    // item과 ViewHolder를 연결, list_data를 연동
+
+    var data = mutableListOf<list_data>()
+    class ListViewHolder(private val binding: ItemMainListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item : list_data) {
+            binding.tvTitle.text = item.tv_title
+            binding.tvContent.text = item.tv_content
+        }
+    }
+
+    // view 공간 만들기, fragment에서 보여주도록 설정할 곳, inflate
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemMainListBinding.inflate(LayoutInflater.from(parent.context), parent , false)
+
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+
+        // holder에 데이터 연결
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+}
--- app/src/main/java/com/now/three_days/ui/main/MainFragment.kt
+++ app/src/main/java/com/now/three_days/ui/main/MainFragment.kt
@@ -6,21 +6,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.now.three_days.R
+import com.now.three_days.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+
+    lateinit var ListAdapter : ListAdapter
+    val data = mutableListOf<list_data>()
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    private var _binding : MainFragmentBinding? = null
 
+    private val binding get() = _binding!!
+
+    // mainFragment에서 만들어둔 view를 보여주도록 연결하기
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+//        return inflater.inflate(R.layout.main_fragment, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ListAdapter = ListAdapter(this)
+
+        data.apply {
+            add(list_data("","1L 마시기", "1L 마시기"))
+            add(list_data("","1km 달리기", "1km 달리기"))
+            add(list_data("","1시간 취미생활 즐기기", "1시간 취미생활 즐기기"))
+
+            // 만들어둔 Adapter에 data 연결하기
+            ListAdapter.data = data
+        }
+
+        // 리사이클러뷰 adapter를 만들어놓은 ListAdapter로 사용하겠다
+        binding.rcList.adapter = ListAdapter
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
