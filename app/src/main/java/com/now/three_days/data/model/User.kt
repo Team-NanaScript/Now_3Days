57b40fec437ef1ec873af4a8c66c011fdd689c8d
sk421120 <sk421120@naver.com>
2021-12-07 15:31:42 +0900

    login적용중

--- app/src/main/java/com/now/three_days/data/model/User.kt
+++ app/src/main/java/com/now/three_days/data/model/User.kt
@@ -1,13 +1,9 @@
 package com.now.three_days.data.model
 
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
+data class User(
     val userId: String,
     val nikname: String,
     val password: String,
     val role: String,
     val email: String,
-    val isLogin: Boolean
 )
--- app/src/main/java/com/now/three_days/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/now/three_days/ui/login/LoginViewModel.kt
@@ -23,7 +23,7 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
         if (result is Result.Success) {
             _loginResult.value =
-                LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+                LoginResult(success = LoggedInUserView(displayName = result.data.nikname))
             return true
         } else {
             _loginResult.value = LoginResult(error = R.string.login_failed)
