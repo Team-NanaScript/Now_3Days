915894a9b2f6af41cc3285947e4b1fe0f5289d5e
kimyoungjin98 <gyu250@naver.com>
2021-12-07 17:39:18 +0900

    추가하기

---
+++ app/src/main/java/com/now/three_days/data/model/ChallengeVO.kt
@@ -0,0 +1,13 @@
+package com.now.three_days.data.model
+
+data class ChallengeVO(
+
+    var c_userId:String,
+    var c_title:String,
+    var c_sDate:String,
+    var c_eDate:String,
+    var c_content:String,
+    var c_progress:Boolean,
+    var c_image:String,
+
+)
