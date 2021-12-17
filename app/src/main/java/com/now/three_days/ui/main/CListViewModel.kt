8844496584e04660eb76094b3f6e3e871cc8fbbe
Nanask <ybhag1005@daum.net>
2021-12-17 16:10:16 +0900

    All List

---
+++ app/src/main/java/com/now/three_days/ui/main/CListViewModel.kt
@@ -0,0 +1,7 @@
+package com.now.three_days.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class CListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/now/three_days/ui/main/RListViewModel.kt
@@ -0,0 +1,7 @@
+package com.now.three_days.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class RListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
