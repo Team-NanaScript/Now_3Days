4a011c2c1d6150d53cef6b1a54d8b81bf8dfcee5
kimyoungjin98 <gyu250@naver.com>
2021-12-20 14:19:54 +0900

    수정^ㅡ^

--- app/src/main/java/com/now/three_days/data/model/RelayVO.kt
+++ app/src/main/java/com/now/three_days/data/model/RelayVO.kt
@@ -2,13 +2,13 @@ package com.now.three_days.data.model
 
 data class RelayVO(
 
-    var key:String = "",
-    var c_userId:String = "",
-    var c_title:String = "",
-    var c_sDate:String = "",
-    var c_eDate:String = "",
-    var c_content:String = "",
-    var c_progress:Boolean = false,
-    var c_image:String = "",
+    var r_seq:String = "",
+    var r_userId:String = "",
+    var r_title:String = "",
+    var r_sDate:String = "",
+    var r_eDate:String = "",
+    var r_content:String = "",
+    var r_progress:Boolean = false,
+    var r_image:String = "",
 
 )
