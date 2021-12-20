2cf44154571f351be446bb6925f8e96ff01b9037
kimyoungjin98 <gyu250@naver.com>
2021-12-20 14:14:56 +0900

    vo 추가

--- app/src/main/java/com/now/three_days/ui/insert/InsertFragment.kt
+++ app/src/main/java/com/now/three_days/ui/insert/InsertFragment.kt
@@ -1,6 +1,9 @@
 package com.now.three_days.ui.insert
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.icu.util.GregorianCalendar
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -42,6 +45,34 @@ class InsertFragment() : Fragment() {
     ): View? {
         _binding = InsertFragmentBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(InsertViewModel::class.java)
+
+        // insert 에서 시작일을 오늘 날짜로 보여주기
+        val today = GregorianCalendar()
+        val year = today.get(Calendar.YEAR)
+        val month = today.get(Calendar.MONTH)
+        val date = today.get(Calendar.DATE)
+        // 왜인지 모르겠으나 month+1 안하면 날짜가 오늘 날짜로 안나옴..
+        binding.sDate.setText("${year}년 ${month + 1}월 ${date}일")
+
+        // 종료일을 클릭했을 때
+        binding.btnEDate.setOnClickListener{
+
+            // 종료일 날짜를 선택하기 위한 코드
+            val calendar = DatePickerDialog(requireContext(), object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view : DatePicker?, y : Int, m : Int, dayofMonth: Int) {
+                    binding.eDate.setText("${ y }년 ${ m +1 }월 ${ dayofMonth }일")
+                }
+            }, year, month, date)
+
+            calendar.show()
+        }
+
         // select_box(spinner) 바인딩 후 어댑터 연결
         var spinner:Spinner = binding.insertSpinner
         var button: Button =  binding.btnSend
@@ -54,59 +85,52 @@ class InsertFragment() : Fragment() {
         )
         spinner.adapter = spinnerAdapter
 
+        // 저장 버튼 클릭
         button.setOnClickListener(View.OnClickListener {
             cs = ChallengeServiceImplV1()
             onClick(binding)
             cs.select()
         })
-
-        return binding.root
     }
 
     // VO에 데이터 넣고 db에 추가하는 method
     fun onClick(binding: InsertFragmentBinding){
         val title:String = binding.title.text.toString()
-        val date:String = binding.date.text.toString()
+        val sDate:String = binding.sDate.text.toString()
+        val eDate:String = binding.eDate.text.toString()
         val content:String = binding.content.text.toString()
 
         // select box
         val spinner:Spinner = binding.insertSpinner
         val select_text = spinner.selectedItem.toString()
 
-        // 12-16일자 토스트 안나옴 왜 ?
         if(select_text == "" || select_text == null){
-            Toast.makeText(requireContext(), "카테고리를 선택하세요 !", Toast.LENGTH_SHORT )
+            Toast.makeText(requireContext(), "카테고리를 선택하세요 !", Toast.LENGTH_SHORT ).show()
             return
         }
         if(title == "" || title == null){
-            Toast.makeText(requireContext(), "제목을 입력하세요 !", Toast.LENGTH_SHORT )
+            Toast.makeText(requireContext(), "제목을 입력하세요 !", Toast.LENGTH_SHORT ).show()
             return
         }
-        if(date == "" || date == null){
-            Toast.makeText(requireContext(), "날짜를 선택하세요 !", Toast.LENGTH_SHORT )
+        if(eDate == "" || eDate == null){
+            Toast.makeText(requireContext(), "날짜를 선택하세요 !", Toast.LENGTH_SHORT ).show()
             return
         }
 
         var challenge:ChallengeVO = ChallengeVO(
             c_title = title,
             c_content = content,
-            c_sDate = date,
+            c_sDate = sDate,
             c_userId = "영진",
-            c_eDate = "",
+            c_eDate = eDate,
             c_progress = false,
             c_image = "",
         )
         cs.insert(challenge, select_text)
 
         binding.title.setText("")
-        binding.date.setText("")
+        binding.eDate.setText("")
         binding.content.setText("")
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(InsertViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
